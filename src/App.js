73e788f778ad8a89f82ce53d06f51738f2d89ef5
WINDOWS-M8A9A01\User <soltanovskiy@brights.com.ua>
2018-12-12 18:00:40 +0200

    Menu open/close functionality, added svg circle, markup done

--- src/App.js
+++ src/App.js
@@ -6,39 +6,117 @@ import {ReactComponent as SearchIcon}  from './assets/icons/search.svg';
 import {ReactComponent as NotificationIcon}  from './assets/icons/notification.svg';
 import {ReactComponent as SettingsIcon}  from './assets/icons/settings.svg';
 import {ReactComponent as LocationIcon}  from './assets/icons/location.svg';
+import {ReactComponent as Circle}  from './assets/icons/circle.svg';
 import './assets/scss/App.scss';
 
 class App extends Component {
-  toggleMenu = () => {
-
-  }
   componentDidMount(){
-    
+    // IE11 forEach polyfill
+    if (window.NodeList && !NodeList.prototype.forEach) {
+      NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    // like jQuery parent
+    Element.prototype.parent = function(selector) {
+      var parent;
+      var elem = this;
+      var ishaveselector = selector !== undefined;
+     
+      while ((elem = elem.parentElement) !== null) {
+        if (elem.nodeType !== Node.ELEMENT_NODE) {
+          continue;
+        }
+     
+        if (!ishaveselector || elem.matches(selector)) {
+          return parent;
+        }
+      }
+    };
+
+    const iconWrapperNodes = document.querySelectorAll('.icon__wrapper');
+    iconWrapperNodes.forEach( item => item.addEventListener('click', function(e) {
+      console.log(this);
+    }))
+
+    const menuNode = document.querySelector('.menu');
+    const toggleMenuHandler = function(e) {
+      if (menuNode.classList.toggle('opened')) {
+
+      } 
+      console.log('toggleMenu')
+    }
+    const menuToggleNode = document.querySelector('.menu__toggler');
+    menuToggleNode.addEventListener('click', toggleMenuHandler)
   }
   render() {
     return (
       <div className="App">
         <div className="menu__wrapper">
           <nav className="menu">
-            <div className="menu__item menu__toggle" onClick={this.toggleMenu}>
-                <MenuIcon class="icon_menu icon_open" alt="menu" />
-                <CloseIcon class="icon_menu icon_close" alt="close" />
+            <div className="menu__toggler">
+                <MenuIcon className="icon_item icon_burger" alt="menu" />
+                <CloseIcon className="icon_item icon_close" alt="close" />
             </div>
-            <ul className="menu__list">
-              <li className="menu__item menu__item_home">
-                <HomeIcon class="icon_item" alt="home" />
+            <ul className="list_menu">
+              <li className="menu__item home">
+                <div className="circle__wrapper wrapper_rotate">
+                  <Circle className="icon_circle" />
+                </div>
+                <div className="wrapper_rotate">
+                  <div className="wrapper_translate">
+                    <div className="icon__wrapper">
+                      <HomeIcon className="icon_item" alt="home" />
+                    </div>
+                  </div>
+                </div>
               </li>
-              <li className="menu__item menu__item_search">
-                <SearchIcon class="icon_item" alt="search" />
+              <li className="menu__item search">
+                <div className="circle__wrapper wrapper_rotate">
+                  <Circle className="icon_circle" />
+                </div>
+                <div className="wrapper_rotate">
+                  <div className="wrapper_translate">
+                    <div className="icon__wrapper">
+                      <SearchIcon className="icon_item" alt="search" />
+                    </div>
+                  </div>
+                </div>
               </li>
-              <li className="menu__item menu__item_notification">
-                <NotificationIcon class="icon_item" alt="notification" />
+              <li className="menu__item notification">
+                <div className="circle__wrapper wrapper_rotate">
+                  <Circle className="icon_circle" />
+                </div>
+                <div className="wrapper_rotate">
+                  <div className="wrapper_translate">
+                    <div className="icon__wrapper">
+                      <NotificationIcon className="icon_item" alt="notification" />
+                    </div>
+                  </div>
+                </div>
               </li>
-              <li className="menu__item menu__item_settings">
-                <SettingsIcon class="icon_item" alt="settings" />
+              <li className="menu__item settings">
+                <div className="circle__wrapper wrapper_rotate">
+                  <Circle className="icon_circle" />
+                </div>
+                <div className="wrapper_rotate">
+                  <div className="wrapper_translate">
+                    <div className="icon__wrapper">
+                      <SettingsIcon className="icon_item" alt="settings" />
+                    </div>
+                  </div>
+                </div>
               </li>
-              <li className="menu__item menu__item_location">
-                <LocationIcon class="icon_item" alt="location" />
+              <li className="menu__item location">
+                <div className="circle__wrapper wrapper_rotate">
+                  <Circle className="icon_circle" />
+                </div>
+                <div className="wrapper_rotate">
+                  <div className="wrapper_translate">
+                    <div className="icon__wrapper">
+                      <LocationIcon className="icon_item" alt="location" />
+                    </div>
+                  </div>
+                </div>
               </li>
             </ul>
           </nav>
